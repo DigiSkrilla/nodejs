1c3773d1341dbd32022a371989f702b51970e635
Macbook Pro <macbookpro@MRIs-Macbook.local>
2023-07-02 15:27:30 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+
+/*  "This is a basic server setup.
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('<h1>Welcome to my page bitches!!!<h1>');
+});
+
+server.listen(3000, '127.0.0.1', () => {
+    console.log('Server Running...');
+});
+
+*/
+const { v4:uuidv4 } = require('uuid');
+
+console.log(uuidv4()); 
+
+
